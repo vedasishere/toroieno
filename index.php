032059110c64c6fe1020b41781bbbb2a20f08f10
w3schools-text <vedas.webinar@gmail.com>
2022-07-12 19:18:31 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>profile</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+    <div class="container">
+        <div class="banner"></div>
+        <div class="nav_menu">
+            <ul>
+                <li class="introduce">
+                    <a href="#introduce">Giới thiệu</a>
+                </li>
+                <li class="info">
+                    <a href="#">Thông tin cá nhân</a>
+                </li>
+                <li class="contact">
+                    <a href="#">Liên hệ</a>
+                </li>
+                <li class="project">
+                    <a href="#">xxx</a>
+                </li>
+            </ul>
+        </div>
+        <div class="profile">
+            <div id="introduce">
+                <div id="avatar">
+                    <img src="./image/avatar.jpg" alt="ảnh đại diện" height="200px">
+                </div>
+                <div id="overview">
+                    <p>
+                        Xin chào các bạn, đây là trang cá nhân của Triển!
+                    </p>
+                </div>
+            </div>
+            <div id="info"></div>
+            <div id="contact"></div>
+            <div id="project"></div>
+        </div>
+    </div>
+</body>
+</html>
